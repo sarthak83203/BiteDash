7ce79e4b1ed36a30f53ecaed41d70603035afec4
sarthak <sarthak.shinde2006@gmail.com>
2025-12-16 22:15:23 +0530

    UserAuthentication for register

--- Server/models/userModel.js
+++ Server/models/userModel.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose"
 //This i am making model in database not in frontend
 const UserSchema=new mongoose.Schema({
-    name:{type:Number,required:true},
+    name:{type:String,required:true},
     email:{type:String,required:true,unique:true},
     password:{type:String,required:true},
     cartData:{type:Object,default:{}},
