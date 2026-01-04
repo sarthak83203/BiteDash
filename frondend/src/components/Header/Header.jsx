7d59e0c111917639f081e73e1a6d5a419e54ab9d
sarthak <sarthak.shinde2006@gmail.com>
2026-01-04 21:03:20 +0530

    Done

--- frondend/src/components/Header/Header.jsx
+++ frondend/src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ export default function Header(){
     return(
         <div className="header">
             <div className="header-contents">
-                <h2 className="h2">Order your favourite Food</h2>
+                <h2 className="h2">Best Food of World</h2>
                 <p>Choose from the Available Foods which are Available for Todays Itself</p>
                 <button>View Menu</button>
             </div>
